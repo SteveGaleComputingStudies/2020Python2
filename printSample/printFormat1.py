9a641b74e5759876d6f68289c54af846c93e591a
Steve Gale <steve.gale@hotmail.com>
2020-03-06 10:46:46 +1100

    first commit

---
+++ printSample/printFormat1.py
@@ -0,0 +1,8 @@
+value0 = 3.14959
+value1 = 6.66666
+print('%.3f  value 1 = %.3f'%(value0,value1))
+
+print('value 0 = {0:.2f}  value 1 = {0:.2f} '.format(value0,value1))
+
+age = 16.4
+print('age = {0:.0f}  '.format(age))
---
+++ while/checkKeys.py
@@ -0,0 +1,6 @@
+while True :
+    keyIn = input("q to quit")
+    print(keyIn)
+    #RUN THE PROGRAM
+    if keyIn.lower() == "q":
+        break
