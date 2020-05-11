9deb029a793b12b2c799097737a9aa10ea9e27ca
Steve Gale <steve.gale@computingstudies.net>
2020-05-11 14:44:07 +1000

    updates 110520

---
+++ for/countMultiplesof5.py
@@ -0,0 +1,7 @@
+#
+sum = 0
+for x in range(5,100,5):
+    print(x)
+    sum = sum + x
+
+print("total of {1}'s is {0}".format(sum,5))
---
+++ functions/handout2/SimpleParameters3.py
@@ -0,0 +1,11 @@
+
+def calculateSellingPrice(costPrice):
+    print("costPrice = {}".format(costPrice))
+    print("sellingPrice = {}".format(costPrice * 2))
+
+
+calculateSellingPrice(22.02)
+calculateSellingPrice(18.99)
+calculateSellingPrice(12.20)
+calculateSellingPrice(99.99)
+
