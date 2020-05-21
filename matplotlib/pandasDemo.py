be7b6b87f15caabae50f5324008e6d33945cf7eb
Steve Gale <steve.gale@computingstudies.net>
2020-05-22 09:42:13 +1000

    updated 220520

---
+++ matplotlib/pandasDemo.py
@@ -0,0 +1,19 @@
+# Import pandas package 
+import pandas as pd 
+  
+# Define a dictionary containing employee data 
+data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
+        'Age':[27, 24, 22, 32], 
+        'Address':['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'], 
+        'Qualification':['Msc', 'MA', 'MCA', 'Phd']} 
+  
+# Convert the dictionary into DataFrame  
+df = pd.DataFrame(data) 
+  
+# select two columns 
+dfNameQual = df[['Name', 'Qualification']]
+
+
+df['agePlus'] = df['Age'] + 20
+
+print(df)
--- tkinter/2F-plotPandasDataFrameExcelRoomDataFilter.py
+++ tkinter/2F-plotPandasDataFrameExcelRoomDataFilter.py
@@ -2,6 +2,7 @@
 # https://datatofish.com/import-csv-file-python-using-pandas/
 # https://datatofish.com/read_excel/
 # https://cmdlinetips.com/2018/02/how-to-subset-pandas-dataframe-based-on-values-of-a-column/
+# https://www.geeksforgeeks.org/dealing-with-rows-and-columns-in-pandas-dataframe/
 
 # pandas DataFrame data filtering
 
@@ -22,8 +23,9 @@ print(df)
 # Filter example: we want only Time and temperature for room E223
 
 #filter unnecesary columns , filter all rooms except E223
-# SELECT * WHERE Room == "E223"
-
+# SELECT Room,Temperature WHERE Room == "E223"
+#print(df[['Time', 'Temperature']]) 
+#
 dfE223Temp = df[df["Room"] == "E223"]
 print(dfE223Temp)
 # ORDER BY Time ACSENDING
@@ -31,4 +33,12 @@ dfE223tempSorted = dfE223Temp.sort_values("Time")
 print(dfE223tempSorted)
 
 dfE223tempSorted.plot(x ='Time', y='Temperature', kind = 'bar')
+
+#SELECT Time, Temperature FROM roomData WHERE Room == 'E208' ORDER BY Time ASCENDING
+
+dfE208Temp = df[df["Room"] == "E208"]               # SELECT * WHERE  Room == E208
+dfE208Temp = dfE208Temp[['Time', 'Temperature']]    # SELECT Time , Temperature
+dfE208Temp = dfE208Temp.sort_values("Time")   # ORDER BY Time ASCENDING
+print(dfE208Temp)
+dfE208Temp.plot(x ='Time', y='Temperature', kind = 'bar')
 plt.show()
