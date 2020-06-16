728fe118e4a8f73bdbc73ff7f615c3017804d9e2
Steve Gale <steve.gale@computingstudies.net>
2020-06-16 10:50:28 +1000

    test ok

--- threading/subprocessTest.py
+++ threading/subprocessTest.py
@@ -1,6 +1,4 @@
 
-#import subprocess
-#r = subprocess.run(["dir", "/w"],capture_output=True) 
-#print(r.stdout)
-
-#subprocess.run()
+import subprocess
+r = subprocess.run([r'C:\Windows\System32\ping.exe', '10.0.0.138'],capture_output=True) 
+print(r.stdout)
